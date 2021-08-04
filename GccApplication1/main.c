236cc3b93e3d9269c531c7ce3ee435ddcd526085
Marius <carp.marius07@gmail.com>
2021-08-04 22:09:33 +0300

    50% duty cycle

--- GccApplication1/main.c
+++ GccApplication1/main.c
@@ -1,18 +1,16 @@
-/*
- * GccApplication1.c
- *
- * Created: 04-Aug-21 9:15:42 PM
- * Author : gamer
- */ 
-
 #include <avr/io.h>
 
 
 int main(void)
 {
+	//set DDRB high to read signal from PB4 (OC0)
     DDRB |= (1<<PB4);
+	
+	//Non-inverting - fast PWM - clk/1
 	TCCR0 = (1<<COM01) | (1<<WGM01) | (1<<WGM00) | (1<<CS00);
-	OCR0 = 178;
+	
+	//50% duty cycle
+	OCR0 = 127;
 	
     while (1) 
     {
